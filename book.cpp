0723bb798dead5e35595a194d960a5e10df99c83
Nicholas Chaves <tile27@proton.me>
2026-01-16 13:59:38 -0500

    feat/added essential properties and methods as well as constructor for book class

--- book.cpp
+++ book.cpp
@@ -1,2 +1,64 @@
 #include "book.h"
 
+Book::Book(){
+    title = "Unknown";
+    author = "Unknown";
+    pubDate = "Unknown";
+    startReadDate = "Unknown";
+    endReadDate = "Unknown";
+    readStatus = "Unknown";
+}
+Book::Book(std::string t, std::string a, std::string pDate, std::string sReadDate, std::string eReadDate, std::string rStatus ){
+    title = t;
+    author = a;
+    pubDate = pDate;
+    startReadDate = sReadDate;
+    endReadDate = eReadDate;
+    readStatus = rStatus;
+}
+
+// setter methods
+void Book::setTitle(std::string newTitle){
+    title = newTitle;
+}
+void Book::setAuthor(std::string newAuthor){
+    author = newAuthor;
+}
+void Book::setPubDate(std::string newPubDate){
+    pubDate = newPubDate;
+}
+void Book::setStartReadDate(std::string newStartReadDate){
+    startReadDate = newStartReadDate;
+}
+void Book::setEndReadDate(std::string newEndReadDate){
+    endReadDate = newEndReadDate;
+}
+void Book::setReadStatus(int newReadStatus){
+    switch(newReadStatus){
+        case 1: readStatus = "To-Read"; break;
+        case 2: readStatus = "Reading"; break;
+        case 3: readStatus = "Have Read"; break;
+        case 4: readStatus = "Did Not Finish"; break;
+        default: readStatus = "To-Read"; 
+    }
+}
+
+// getter methods
+std::string Book::getTitle() const{
+    return title;
+}
+std::string Book::getAuthor() const{
+    return author;
+} 
+std::string Book::getPubDate() const{
+    return pubDate;
+} 
+std::string Book::getStartReadDate() const{
+    return startReadDate;
+}
+std::string Book::getEndReadDate() const{
+    return endReadDate;
+}
+std::string Book::getReadStatus() const{
+    return readStatus;
+}
--- book.h
+++ book.h
@@ -4,15 +4,29 @@
 class Book{
 private:
 	std::string title;
-    
+    std::string author;
+    std::string pubDate; // book publish date
+    std::string startReadDate;
+    std::string endReadDate;
+    std::string readStatus;
+
 public:
+    Book();
+    Book(std::string t, std::string a, std::string pDate, std::string sReadDate, std::string eReadDate, std::string rStatus );
+
     // setter methods
-    void setTitle(std::string newTitle){
-        title = newTitle;
-    }
+    void setTitle(std::string newTitle);
+    void setAuthor(std::string newAuthor);
+    void setPubDate(std::string newPubDate);
+    void setStartReadDate(std::string newStartReadDate);
+    void setEndReadDate(std::string newEndReadDate);
+    void setReadStatus(int newReadStatus);
 
     // getter methods
-    std::string getTitle(){
-        return title;
-    }
+    std::string getTitle() const;
+    std::string getAuthor() const;
+    std::string getPubDate() const;
+    std::string getStartReadDate() const;
+    std::string getEndReadDate() const;
+    std::string getReadStatus() const;
 };
