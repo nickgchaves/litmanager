91019b73136fe3321f157250adb8dba3270341e3
Nicholas Chaves <tile27@proton.me>
2026-01-16 12:57:47 -0500

    feat: added title property and methods

--- book.h
+++ book.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <string>
 
 class Book{
 private:
-	
+	std::string title;
+    
 public:
+    // setter methods
+    void setTitle(std::string newTitle){
+        title = newTitle;
+    }
+
+    // getter methods
+    std::string getTitle(){
+        return title;
+    }
 };
